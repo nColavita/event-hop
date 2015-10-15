ab70323d42b6d2a4800d5a35bc8d863046218a68
Nick Colavita <nickcolavita88@gmail.com>
2015-10-15 17:49:28 -0400

    User Update Profile Form now includes an area to add a users first and last name.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
   def after_sign_up_path_for(resources)
   		'/places'
   end
@@ -11,15 +15,19 @@ class ApplicationController < ActionController::Base
 		'/places'
 	end
 
-	# def admin
-
-	# end
+	def configure_permitted_parameters
+		devise_parameter_sanitizer.for(:account_update) { |params|
+	    params.permit(
+	      :email, :password, :password_confirmation, :fname,
+	      :lname, :current_password
+	    )
+	  }
+	end
 
 	def authenticate_admin!
 		unless current_user[params[:admin]] == true
 			
 		end
 	end
-
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151014160527) do
+ActiveRecord::Schema.define(version: 20151015213131) do
 
   create_table "events", force: :cascade do |t|
     t.integer  "user_id"
@@ -66,6 +66,8 @@ ActiveRecord::Schema.define(version: 20151014160527) do
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.boolean  "admin",                  default: false
+    t.string   "fname"
+    t.string   "lname"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
