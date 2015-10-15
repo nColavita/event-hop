c5bcafc63fdffe7b991a26c7224656f3c637851d
Leah W. Chang <leahwchang@gmail.com>
2015-10-15 15:28:34 -0400

    add heroku domain to email hosting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+  config.action_mailer.default_url_options = { :host => "http://eventhop.herokuapp.com" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :port =>           '587',
+    :address =>        'smtp.mandrillapp.com',
+    :user_name =>      ENV['EVENT_HOP_MANDRILL_USERNAME'],
+    :password =>       ENV['EVENT_HOP_MANDRILL_APIKEY'],
+    :authentication => :plain,
+    :domain => "heroku.com",
+    :enable_starttls_auto => true
+    }
 end
