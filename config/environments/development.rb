5c9e6375c5269513ce765e78cc5b4658505296f7
Leah W. Chang <leahwchang@gmail.com>
2015-10-15 15:43:00 -0400

    add config to heroku email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,5 +41,4 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { :host => "localhost: 3000" }
-
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,6 +80,8 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { :host => "http://eventhop.herokuapp.com" }
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.smtp_settings = {
     :port =>           '587',
     :address =>        'smtp.mandrillapp.com',
