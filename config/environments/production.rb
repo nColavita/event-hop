847a088711d148fa4ab5f4c7aae27ec80043e5bf
Leah W. Chang <leahwchang@gmail.com>
2015-11-23 17:13:48 -0500

    add sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,19 +77,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => "http://leah-eventhop.herokuapp.com" }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.smtp_settings = {
-    :port =>           587,
-    :address =>        'smtp.mandrillapp.com',
-    :user_name =>      ENV['EVENT_HOP_MANDRILL_USERNAME'],
-    :password =>       ENV['EVENT_HOP_MANDRILL_APIKEY'],
-    :authentication => :plain,
-    :domain => "http://leah-eventhop.heroku.com",
-    :enable_starttls_auto => true
-    }
 
   config.serve_static_assets = true
 
