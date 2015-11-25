43bb91743801950b741d9f1d3ec9245f39e03801
Leah W. Chang <leahwchang@gmail.com>
2015-11-25 01:00:46 -0500

    set assets precompile to true, delete precompiled assets, rake assets precompile

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,6 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-
 ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
   :port           => '587',
